3e76343edf3b032648eaab5b5ceded25c682c077
atayadwii <atayadwii@gmail.com>
2024-02-28 10:37:13 +0700

    first commit

---
+++ praktikum 02/submit.php
@@ -0,0 +1,18 @@
+<?php
+$nama = $_POST["nama"]; 
+$matkul = $_POST["matkul"];
+$tugas = $_POST["tugas"];
+$uts = $_POST["uts"];
+$uas = $_POST["uas"];
+
+echo "nama lengkap : ". $nama;
+echo "<br>";
+echo "mata kuliah : " . $matkul;
+echo "<br>";
+echo "nilai tugas : " . $tugas;
+echo "<br>";
+echo "nilai uts : " . $uts;
+echo "<br>";
+echo "nilai uas : " . $uas
+
+?>
---
+++ praktikum 02/switch.php
@@ -0,0 +1,21 @@
+<?php
+$grade = "A";
+$predikat; 
+
+switch ($grade) {
+    case "A":
+        $predikat = "sangat baik";
+        break;
+    case "B":
+        $predikat = "baik";
+        break; 
+    case "C":
+        $predikat = "cukup";
+        break;
+    case "D":
+        $predikat = "kurang";
+        break;
+    case "E":
+        $predikat = "wassaalam";
+         break;    
+}
